0196617fe8e493f0ef307b37e605974f50276f22
Vinicius Martins <viniciusmv91@gmail.com>
2021-05-15 19:53:44 -0300

    Configuring services on startup, setting up DbContext to Pomelo provider

---
+++ Supermarket.API/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Supermarket.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Supermarket.API.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
