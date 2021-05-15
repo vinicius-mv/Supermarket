f6c8b077a6b4a205fd8723254d3f2044b6b2a1c7
Vinicius Martins <viniciusmv91@gmail.com>
2021-05-15 19:26:50 -0300

    Add project files.

---
+++ Supermarket.API/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Supermarket.API.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+
+        // ef nav
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Supermarket.API/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Supermarket.API.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public double UnitsInStock { get; set; }
+        public DateTime RecordDate { get; set; }
+
+        // ef nav
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
