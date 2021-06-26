65869feb557ad28cf13e591d852d200ba7bea88e
Vinicius Martins <viniciusmv91@gmail.com>
2021-06-26 15:16:57 -0300

    implement Seeding Service and Apply pending Migrations at startup, remove populateMigration

---
+++ Supermarket.API/Context/SeedingService.cs
@@ -0,0 +1,50 @@
+﻿using Supermarket.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Supermarket.API.Context
+{
+    public class SeedingService
+    {
+        private AppDbContext _context;
+
+        public SeedingService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Seed()
+        {
+            await SeedCategories();
+            await SeedProducts();
+        }
+
+        private async Task SeedCategories()
+        {
+            if (_context.Categories.Any())
+                return;
+
+            var c1 = new Category() { Name = "Beverages", ImageUrl = "images/beverages.jpg" };
+            var c2 = new Category() { Name = "Food takeout", ImageUrl = "images/food-takeout.jpg" };
+            var c3 = new Category() { Name = "Groceries", ImageUrl = "images/groceries.jpg" };
+
+            _context.Categories.AddRange(c1, c2, c3);
+            await _context.SaveChangesAsync();
+        }
+
+        private async Task SeedProducts()
+        {
+            if (_context.Products.Any())
+                return;
+
+            var p1 = new Product() { Name = "Diet-Coke", Description = "Diet-Coke 350 ml", Price = 1.3m, CategoryId = 1 };
+            var p2 = new Product() { Name = "Sandwich", Description = "Sandwich 300 g", Price = 4.50m, CategoryId = 2 };
+            var p3 = new Product() { Name = "Pudding", Description = "Pudding 400 g", Price = 6.0m, CategoryId = 3 };
+
+            _context.Products.AddRange(p1, p2, p3);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- Supermarket.API/Program.cs
+++ Supermarket.API/Program.cs
@@ -3,17 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Supermarket.API.Context;
 
 namespace Supermarket.API
 {
-    public static class Program
+    public class Program
     {
-        public static void Main(string[] args)
+        public async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            // seed data and apply pending migrations to the database at startup
+            try
+            {
+                var context = services.GetRequiredService<AppDbContext>();
+                await context.Database.MigrateAsync(); 
+                await new SeedingService(context).Seed();
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured during migration");
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
